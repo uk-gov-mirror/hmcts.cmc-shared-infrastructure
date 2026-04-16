09ff80d270bb74c1e98d1e1e81fd61a23bce7f8c
Ryan McAlary <ryan.mcalary@justice.gov.uk>
2026-04-16 09:15:14 +0100

    DTSPO-30208 - Grant new Jenkins MIs access to keyvault

--- application-insights.tf
+++ application-insights.tf
@@ -1,5 +1,5 @@
 module "application_insights" {
-  source = "git@github.com:hmcts/terraform-module-application-insights?ref=master"
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
 
   env                 = var.env
   product             = var.product
